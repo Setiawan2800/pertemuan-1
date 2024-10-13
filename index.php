abf6036f2fd51b6e4f5f149d5b3ac7ffdbfbe7fc
Setiawan2800 <ekasetiawan2800@gmail.com>
2024-10-13 10:27:21 +0700

    Pertama

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    echo "Hello world";
+    $sepuh = "Juli";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pertemuan 1</title>
+</head>
+<body>
+
+    <h1>Pertemuan 1</h1>
+    <h2>Hello World</h2>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non nemo, aliquam eum quod nesciunt debitis. Fugiat autem quasi veritatis provident quo, ut ipsam error, alias dignissimos nesciunt ea quaerat, vero tempore saepe harum ratione. Facere, optio voluptates. Ullam perferendis voluptas rem eligendi quam illo? Vel ullam vitae alias voluptate autem.</p>
+
+</body>
+</html>
